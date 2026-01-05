5a346abc2ebb432f16de1b7455e280f5c26f317b
Rishu Kesharwani <rishukesharwani480@gmail.com>
2026-01-05 22:02:28 +0530

    Fix EJS layout structure and listings pages

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose")
+const methodOverride=require("method-override")
 const path=require("path");
+
 const Listing=require("./models/listing.js");
+const ejsMate = require("ejs-mate");
 
 const port=8080;
 const MONGO_url="mongodb://127.0.0.1:27017/wanderlust";
@@ -17,10 +20,12 @@ main().then(()=>{
 async function main(){
     await mongoose.connect(MONGO_url);
 }
-
+app.engine("ejs",ejsMate);
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname,"/public"))) 
 
 app.get("/testListing",async (req,res)=>{
     let sampleListing=new Listing({
@@ -49,7 +54,7 @@ app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs");
 });
 
-//create route
+
 // CREATE route
 app.post("/listings", async (req, res) => {
     console.log(req.body);           // check data coming
@@ -67,7 +72,27 @@ app.get("/listings/:id",async(req,res)=>{
     res.render("listings/show",{listing})
 });
 
+//edit route
+app.get("/listings/:id/edit",async (req,res)=>{
+       let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+})
 
+//update route
+app.put("/listings/:id",async (req,res)=>{
+    let {id}=req.params;
+   await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+
+//delete raute
+app.delete("/listings/:id",async (req,res)=>{
+     let {id}=req.params;
+   let deletedListing=await Listing.findByIdAndDelete(id);
+   console.log(deletedListing);
+   res.redirect("/listings");
+})
 
 app.listen(port,()=>{
     console.log(`this is listening to port ${port}`)
